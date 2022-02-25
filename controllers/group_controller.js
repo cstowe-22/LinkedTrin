4d90409f92c16bf3ad0999f10dc68559eb9ea457
NicholasEng22 <61562438+NicholasEng22@users.noreply.github.com>
2022-02-25 09:34:27 -0500

    Whatever

--- controllers/group_controller.js
+++ controllers/group_controller.js
@@ -2,6 +2,7 @@ const express = require('express'),
   router = express.Router();
 
 const Group = require('../models/group_model');
+const User = require('../models/user_model');
 
 router.get('/groupListings', function(request, response) {
     let groups = Group.getAllGroups();
@@ -10,9 +11,19 @@ router.get('/groupListings', function(request, response) {
     for(name in groups){
       groupArray.push(groups[name])
     }
+
+    let users = User.getAllUsers();
+    let userObj = User.getAllUsers();
+    let userArray = [];
+    response.status(200);
+    for(id in users){
+      userArray.push(users[id])
+    }
+
     response.setHeader('Content-Type', 'text/html')
     response.render("groupListings", {
-      groups: groupArray
+      groups: groupArray,
+      users: userObj
     });
 });
 
