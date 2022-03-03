ccb083dbc55435518ca08fac582cecd96856060b
NicholasEng22 <61562438+NicholasEng22@users.noreply.github.com>
2022-03-02 19:58:24 -0500

    Fixed the expanded view

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -51,5 +51,32 @@ router.get('/eventListings/:type', function(request, response) {
       });
 });
 
+router.get('/event/:path', function(request, response) {
+    let events = Event.getAllEvents();
+    let eventObj = Event.getAllEvents();
+    let selectedEvent = {};
+    let path = request.params.path;
+    for(eventEntry in events){
+      if(events[eventEntry].path==path){
+        selectedEvent = events[eventEntry];
+      }
+    }
+    let users = User.getAllUsers();
+    let userObj = User.getAllUsers();
+    let userArray = [];
+    response.status(200);
+    for(id in users){
+      userArray.push(users[id])
+    }
+    // TODO: add logic after user is figured route
+    let followed = 0;
+      response.setHeader('Content-Type', 'text/html')
+      response.render("event",{
+        event: selectedEvent,
+        users: userObj,
+        followed: followed
+      });
+});
+
 
 module.exports = router;
