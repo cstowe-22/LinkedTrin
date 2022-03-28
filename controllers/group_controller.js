06c2f598e7e6031dacb1b96110cbf902d15a81fd
cstowe-22 <56417887+cstowe-22@users.noreply.github.com>
2022-03-28 12:17:32 -0400

    Changes

--- controllers/group_controller.js
+++ controllers/group_controller.js
@@ -1,28 +1,19 @@
 const express = require('express'),
   router = express.Router();
   const app = express();
-    const fs = require('fs');
 
 const Group = require('../models/group_model');
 const User = require('../models/user_model');
 
-function loggedIn(request, response, next) {
-  if (request.user) {
-    next();
-  } else {
-    response.redirect('/login');
-  }
-}
-
-router.get('/groupListings', loggedIn, async function(request, response) {
-    let groups = await Group.getAllGroups();
-    let groupObj = await Group.getAllGroups();
+router.get('/groupListings', function(request, response) {
+    let groups = Group.getAllGroups();
+    let groupObj = Group.getAllGroups();
     let groupArray = [];
     for(name in groups){
       groupArray.push(groups[name])
     }
-    let users = await User.getAllUsers();
-    let userObj = await User.getAllUsers();
+    let users = User.getAllUsers();
+    let userObj = User.getAllUsers();
     let userArray = [];
     response.status(200);
     for(id in users){
@@ -39,9 +30,9 @@ router.get('/groupListings', loggedIn, async function(request, response) {
     });
 });
 
-router.get('/groupListings/'+':type', loggedIn, async function(request, response) {
-    let groups = await Group.getAllGroups();
-    let groupObj = await Group.getAllGroups();
+router.get('/groupListings/'+':type', function(request, response) {
+    let groups = Group.getAllGroups();
+    let groupObj = Group.getAllGroups();
     let groupList = [];
     let typeSave = request.params.type;
     for(type in groups){
@@ -49,8 +40,8 @@ router.get('/groupListings/'+':type', loggedIn, async function(request, response
         groupList.push(groups[type]);
       }
     }
-    let users = await User.getAllUsers();
-    let userObj = await User.getAllUsers();
+    let users = User.getAllUsers();
+    let userObj = User.getAllUsers();
     let userArray = [];
     response.status(200);
     for(id in users){
@@ -65,28 +56,24 @@ router.get('/groupListings/'+':type', loggedIn, async function(request, response
       });
 });
 
-router.get('/group/:path', loggedIn, async function(request, response) {
-    let groups = await Group.getAllGroups();
-    let groupObj = await Group.getAllGroups();
-    let userId = await User.getId(request.user._json.email);
+router.get('/group/:path', function(request, response) {
+    let groups = Group.getAllGroups();
+    let groupObj = Group.getAllGroups();
     let selectedGroup = {};
-    let isLeader = false;
     let path = request.params.path;
     for(groupEntry in groups){
       if(groups[groupEntry].path==path){
         selectedGroup = groups[groupEntry];
-        isLeader = await Group.isLeader(userId, selectedGroup.path);
       }
     }
-    let users = await User.getAllUsers();
-    let userObj = await User.getAllUsers();
+    let users = User.getAllUsers();
+    let userObj = User.getAllUsers();
     let userArray = [];
+
     response.status(200);
     for(id in users){
       userArray.push(users[id])
     }
-    console.log("My group: ");
-    console.log(selectedGroup);
     // TODO: add logic after user is figured route
     let followed = 0;
       response.setHeader('Content-Type', 'text/html')
@@ -95,56 +82,9 @@ router.get('/group/:path', loggedIn, async function(request, response) {
         users: userObj,
         userArray: userArray,
         followed: followed,
-        user: request.user,
-        isLeader: isLeader
+        status:
+        user: request.user
       });
-});
-
-router.get('/groupCreation', loggedIn, async function(request, response) {
-    response.status(200);
-    response.setHeader('Content-Type', 'text/html');
-    let users = await User.getAllUsers();
-    response.render("groupCreation",{
-      user: request.user,
-      users: users
-    }
-  );
-});
-router.post('/groupCreation', loggedIn, async function(request, response) {
-    let groupName = request.body.groupName;
-    let path = groupName.replace(' ', '-').toLowerCase();
-    let leaderList = request.body.leaderList;
-    let advisor = request.body.advisor;
-    let description = request.body.description;
-    let type = request.body.type;
-    let memberList = request.body.memberList;
-
-    console.log("My Grroup: ");
-    console.log(request.body);
-   if(0==0){
-      let groups = await JSON.parse(fs.readFileSync('./data/groups.json'));
-      let newGroup = {
-        "name": groupName,
-        "path": path,
-        "leaders": leaderList,
-        "advisor": advisor,
-        "description": description,
-        "type": type,
-        "members": memberList
-      }
-      groups[path] = newGroup;
-      await fs.writeFileSync('./data/groups.json', JSON.stringify(groups));
-      response.status(200);
-      response.setHeader('Content-Type', 'text/html')
-      response.redirect("/groupListings");
-
-   }else{
-     response.status(400);
-     response.setHeader('Content-Type', 'text/html')
-     response.render("error", {
-       "errorCode":"400"
-     });
-   }
 });
 
 module.exports = router;
